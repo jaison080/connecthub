01db5cd24695035241ec2cb9f09f39c9c8932780
jaison080 <jaisondennis080@gmail.com>
2023-03-15 16:22:14 +0530

    Updated

--- src/pages/api/user/friends/mutual.js
+++ src/pages/api/user/friends/mutual.js
@@ -2,12 +2,13 @@ import User from "../../../../models/User";
 import { withAuth } from "../../../../utils/auth";
 import connectDB from "../../../../utils/connectDB";
 async function handler(req, res) {
-  if (req.method === "GET") {
+  if (req.method === "POST") {
     try {
       await connectDB();
       const { uid } = req.user;
+      const { friendId } = req.body;
       const user = await User.findOne({ uid: uid });
-      const userFriends = await User.findOne({ uid: uid }).friends;
+      // const userFriends = await User.findOne({ uid: uid }).friends;
       //   const mutualFriends = [];
       //   for (let i = 0; i < userFriends.length; i++) {
       //     const friend = await User.findOne({ _id: userFriends[i] });
@@ -20,9 +21,14 @@ async function handler(req, res) {
       //   }
       //   mutualFriends = await User.findMany({ uid: { $in: mutualFriends } });
 
+      // const mutualFriends = await User.find({
+      //   _id: { $in: userFriends },
+      //   friends: user._id,
+      // }).populate("friends");
+
       const mutualFriends = await User.find({
-        _id: { $in: userFriends },
-        friends: user._id,
+        _id: { $ne: friendId, $in: (await User.findById(user._id)).friends },
+        friends: { $elemMatch: { $eq: friendId } },
       }).populate("friends");
 
       res
